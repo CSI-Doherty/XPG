1868436d44008c22a01dbeafab817d9e4262b141
Justine Seow <jussjw@gmail.com>
2024-11-29 09:51:01 +1100

    Update XPG_func.R

--- R/XPG_func.R
+++ R/XPG_func.R
@@ -1,46 +1,15 @@
-#' extension of lapply function
-#'
-#' rewrite lapply function so that it returns a named vector
-#' @param X vector names
-#' @param FUN function in lapply
-#' @return A named vector
-#' @export
-lapply2 <- function (X, FUN, ...)  {
-  names <- X
-  FUN <- match.fun(FUN)
-  if (!is.vector(X) || is.object(X)) {
-    X <- as.list(X)
-  }
-  names(X) <- X
-  .Internal(lapply(X, FUN))
-}
-
 #' NN Connection
 #'
 #' Calculating Percentage Neighbourhood Connection 
 #' @return The average score per cell type and average percentage per each cell  
 #' @export
 nnfunc = function(s, graph, label, cat){
-  nn = s@graphs[[graph]]
-  tmpll = data.frame(lapply2(levels(s@meta.data[,label]), function(x){
-    cellid = rownames(s@meta.data[s@meta.data[,label]==x,])
-    print(dim(nn[,colnames(nn) %in% cellid]))
-    return(rowSums(nn[,colnames(nn) %in% cellid]))
-  }), check.names = FALSE)
-  s@meta.data[,label, drop = FALSE] %>% as_tibble(rownames = 'barcodes') %>% left_join(tmpll %>% as_tibble(rownames = 'barcodes')) -> tmpll
-
-  tmpll %>% select(!barcodes) %>% group_by(!!rlang::sym(label)) %>% summarise_each(sum) %>% melt(id.vars = label) %>%
-    group_by(!!rlang::sym(label)) %>% mutate(per =  100 *value/sum(value)) -> m
-  l = m[m[[label]] == m$variable, ]$per
-
-  if(cat == 'group'){
-    return(c(l, mean(l)))
-  }else if(group == 'cell'){
-    ct_ll = as.numeric(apply(tmpll, 1, function(x) x[x[label]]))
-    rs = rowSums(tmpll[,levels(s@meta.data[,label])])
-    avg = sum(ct_ll/rs*100)/dim(tmpll)[1]
-    return(c(l, avg))
-  }
+  l = as.character(s@meta.data[,label])
+  nn = as(s@graphs[[graph]], 'matrix')
+  nn = t(rowsum(t(nn), l))
+  per = mapply(function(x,y) nn[x,y], 1:dim(nn)[1], l)/k*100
+  if(cat == 'group'){per = tapply(per, l, mean)}
+  return(c(per, mean(per)))
 }
 
 #' PCA and NN functions
@@ -97,27 +66,23 @@ XPG = function(g, sp, graph, seu, ct, core, k, cat){
       suppressMessages(suppressWarnings({
         if(j==0){
           df = insider(gi = c(), seu = seu, graph = graph, sp = sp, arr = arr, ct = ct, k = k, cat = cat)
-          class(df) <- "numeric"
           df = t(data.frame(c(df,'sp',0)))
         }else{
-          df = mclapply(g, FUN = mclapplyfunc, mc.cores = core, mc.preschedule = T, seu = seu, graph = graph, sp = sp, arr = arr, ct = ct, k = k, cat = cat)
+          df = mclapply(g[1:3], FUN = mclapplyfunc, mc.cores = core, mc.preschedule = T, seu = seu, graph = graph, sp = sp, arr = arr, ct = ct, k = k, cat = cat)
           if (is.list(df)) df = do.call(rbind, df)
-          class(df) <- "numeric"
           df = cbind(df,g,rep(j,dim(df)[1]))
         }
         colnames(df) = c(levels(seu@meta.data[,ct]),'average','gene','round')
-
       }))
       mer = rbind(mer,df)
-      arr = c(arr, df[which.max(df[,'average']),'gene'])
+      arr = c(arr, df$gene[which.max(df$average)])
       g = g[!g %in% arr]
-
+      
       jet <- Sys.time()
       t = difftime(jet, jst, units = 'secs')
       jt = c(jt,t)
 
       print(sprintf(paste('Time @', j,'genes: %.2f %s'), t, units(t)))
-
     }
   },error=function(e) {
     message('An Error Occurred');print(e)
